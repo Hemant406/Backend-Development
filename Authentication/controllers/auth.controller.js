4c859a8938414e5867bb32355116e3f4a80e0e30
Hemant406 <hemantpande406@gmail.com>
2026-06-22 18:00:25 +0530

    authentication folder added

---
+++ Authentication/controllers/auth.controller.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const userModel = require("../models/user.model.js")
+const jwt = require("jsonwebtoken")
+
+
+async function registerUser(req,res){
+    const {username,email,password} = req.body
+
+    const user = await userModel.create({
+        username,
+        email,
+        password
+    })
+
+    const token = jwt.sign({
+        id:user._id
+    },process.env.JWT_SECRET)
+
+    res.status(201).json({
+        message:"user created successfully",
+        user,
+        token
+    })
+
+}
+
+module.exports = { registerUser }
---
+++ Authentication/server.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = require("./src/app.js")
+const connectDB = require("./db/db.js")
+
+connectDB()
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+    
+})
